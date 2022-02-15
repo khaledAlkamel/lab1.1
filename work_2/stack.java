f5b12dd41942291e421cf11ac3f3f75816485120
khaledAlkamel <98500362+khaledAlkamel@users.noreply.github.com>
2022-02-16 00:02:46 +0300

    Add files via upload

---
+++ work_2/stack.java
@@ -0,0 +1,12 @@
+/**
+ * Created by خالد الكامل on 4/02/22.
+ */
+public interface stack<E> {
+        boolean isEmpty();
+        int size ();
+        E top();
+        void push(E element);
+        E pop();
+    }
+
+
