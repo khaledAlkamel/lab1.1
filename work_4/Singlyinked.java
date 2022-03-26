4f395b51ba2b0aba14ab22194f61d0d1ad698abe
khaledAlkamel <98500362+khaledAlkamel@users.noreply.github.com>
2022-03-26 23:53:25 +0300

    Add files via upload

---
+++ work_4/Singlyinked.java
@@ -0,0 +1,12 @@
+/**
+ * Created by خالد الكامل on 26/03/22.
+ */
+public class Singlyinked {
+    int data;
+    Singlyinked next;
+    Singlyinked(int d)
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
